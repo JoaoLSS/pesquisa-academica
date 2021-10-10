1f285d46a28f3a1166ad4d9f444a59205800775b
joao <joao.lucas@cherre.com>
2021-10-10 16:50:43 -0300

    fix functions port

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-(trap 'kill 0' SIGINT; functions-framework --source analytics/main.py --target hello_world --debug & firebase serve --only functions & npm start --prefix frontend)
+(trap 'kill 0' SIGINT; functions-framework --source analytics/main.py --target hello_world --debug & firebase serve --only functions --port=5005 & npm start --prefix frontend)
