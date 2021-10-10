c5e13092b60a7f7d31b7940f3cd0659cb6dd4b91
joao <joao.lucas@cherre.com>
2021-10-10 14:17:51 -0300

    fix start script

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-functions-framework --source analytics/main.py --target hello_world --debug & 
-firebase serve --only functions &
-npm start --prefix frontend
+(trap 'kill 0' SIGINT; functions-framework --source analytics/main.py --target hello_world --debug & firebase serve --only functions & npm start --prefix frontend)
